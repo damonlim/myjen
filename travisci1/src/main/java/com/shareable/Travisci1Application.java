ad9e87782280cec82c0700cb01d85fd0bc8aebaa
damonlim <damonlim@yahoo.com>
2021-11-14 15:06:32 +0800

    Initial commit

---
+++ travisci1/src/main/java/com/shareable/Travisci1Application.java
@@ -0,0 +1,13 @@
+package com.shareable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Travisci1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Travisci1Application.class, args);
+	}
+
+}
---
+++ travisci1/src/test/java/com/shareable/Travisci1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shareable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Travisci1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
